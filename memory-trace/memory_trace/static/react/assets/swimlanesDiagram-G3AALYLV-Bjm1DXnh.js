c89445b53b8b136bdb324b9c283d378f40bba351
jnl-tshi <jnl_tshibuyi@hotmail.com>
2026-07-30 13:16:16 +0100

    fix: keep main trunk visible in trail Memory-Entry: mse_0v732ccp4tj2p1gf

--- memory-trace/memory_trace/static/react/assets/swimlanesDiagram-G3AALYLV-Bjm1DXnh.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-BFSi5j4-.js";import{_ as a}from"./mermaid.core-BqIzdcgw.js";import"./chunk-5VM5RSS4-Cc7Z5Txu.js";import"./chunk-XXDRQBXY-CJC0yrce.js";import"./chunk-VR4S4FIN-zPeC9ftE.js";import"./chunk-32BRIVSS-FZi5O7tW.js";import"./channel-C9sn8-kr.js";import"./index-CeKTaIJE.js";import"./timer-DWAvo6M8.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ memory-trace/memory_trace/static/react/assets/swimlanesDiagram-G3AALYLV-DAU_VwD8.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-B63cejKW.js";import{_ as a}from"./mermaid.core-C2Lo2C_t.js";import"./chunk-5VM5RSS4-3zUtA0I5.js";import"./chunk-XXDRQBXY-LDIM98Ep.js";import"./chunk-VR4S4FIN-BmJ4Yy3m.js";import"./chunk-32BRIVSS-BIov9BzN.js";import"./channel-Dk4GBsqM.js";import"./index-Dx9r7jom.js";import"./timer-DWAvo6M8.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
