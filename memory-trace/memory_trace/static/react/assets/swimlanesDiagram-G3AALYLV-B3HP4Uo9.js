8c01e1923cc6810e2041693002351150c6d528c5
jnl-tshi <jnl_tshibuyi@hotmail.com>
2026-09-16 16:14:19 +0100

    build: refresh Trace static bundle

---
+++ memory-trace/memory_trace/static/react/assets/swimlanesDiagram-G3AALYLV-B3HP4Uo9.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-D2Gugql7.js";import{_ as a}from"./mermaid.core-DJrKsw1z.js";import"./chunk-5VM5RSS4-DJtdwvEd.js";import"./chunk-XXDRQBXY-DDfNW5Yp.js";import"./chunk-VR4S4FIN-BrZS_YWY.js";import"./chunk-32BRIVSS-CBkD75p4.js";import"./channel-CAg0S6-d.js";import"./index-CvCU9uFg.js";import"./timer-DWAvo6M8.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
